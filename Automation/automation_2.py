dff2ffae85278eaeed4fe3ada6404cfe341ade7d
Youssef Daoud <63427786+MrHowtz@users.noreply.github.com>
2024-02-04 20:29:23 +0100

    Update automation_2.py

--- Automation/automation_2.py
+++ Automation/automation_2.py
@@ -48,7 +48,8 @@ def main():
         last_modified_date_utc = get_last_modified(username, repository, file_path, github_token)
 
         if last_modified_date_utc:
-            last_modified_date_tz = last_modified_date_utc.astimezone(pytz.timezone('Europe/Paris'))
+            utc_plus_one = pytz.timezone('Europe/Paris')
+            last_modified_date_tz = last_modified_date_utc.astimezone(utc_plus_one)
             last_modified_date_str = last_modified_date_tz.strftime('%Y-%m-%d %H:%M:%S')
             print(f"File: {file_name}, Last modified: {last_modified_date_str}")
         else:
